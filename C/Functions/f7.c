37ff829236ff5a1b38ea199ffd3203ff9076bea3
ik-awais <mawaisqq@gmail.com>
2025-12-06 18:57:18 +0500

    Modified code

--- C/Functions/f7.c
+++ C/Functions/f7.c
@@ -1,2 +1,30 @@
 // Temperature Converter
 #include <stdio.h>
+void celsius_fahrenheit(float celsius);
+void fahrenheit_celsius(float fahrenheit);
+int main(){
+    int check;
+    float celsius, fahrenheit;
+    printf("\tTemperature conversation\n1. Fahrenheit-Celsius\t2. Celsius-Fahrenheit?\n");
+    scanf("%d", &check);
+    if (check == 1)
+    {
+        printf("Enter value: ");
+        scanf("%f", &fahrenheit);
+        fahrenheit_celsius(fahrenheit);
+    }
+    else
+    {
+        printf("Enter value: ");
+        scanf("%f", &celsius);
+        celsius_fahrenheit(celsius);
+    }
+
+    return 0;
+}
+void celsius_fahrenheit(float celsius){
+    printf("Temperature in Fahrenheit: %.2f\n", (celsius*9/5)+32);
+}
+void fahrenheit_celsius(float fahrenheit){
+    printf("Temperature in Celsius: %.2f\n", (fahrenheit-32)*5/9);
+}
