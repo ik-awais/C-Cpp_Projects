48167a0d240715a30a666125b0b67e815960b146
ik-awais <mawaisqq@gmail.com>
2025-12-07 22:21:58 +0500

    Better logic

--- C/Arrays_2D/2D_Array.c
+++ C/Arrays_2D/2D_Array.c
@@ -2,12 +2,11 @@
 #define ROW 5
 #define COL 5
 int main() {
-    int arr_2d[ROW][COL];
+    int arr_2d[ROW][COL] = {0};
     int i, j;
 
     for (i = 0; i < ROW; i++) {
         for (j = 0; j < COL; j++) {
-            arr_2d[i][j] = 0;
             printf("%d ", arr_2d[i][j]);
         }
     printf("\n");
